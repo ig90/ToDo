e7b5f6287b4bce80c395c413e0f1b495e1d48b1b
Igor <igor.filipiuk@gmail.com>
2019-05-25 13:56:29 +0200

    added items,  addItem and removeItem methods

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 
 })
 export class TodoComponent {
+  
+  items: any[] = [
+    'task 1',
+    'task 2',
+    'task 3',
+    'task 4',
+    'task 5',
+    'task 6'
+  ];
 
+  addItem(input: HTMLInputElement) {
+    this.items.splice(0, 0, input.value);
+    input.value = '';
+  }
+
+  removeItem(item) {
+    const index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
 
 }
