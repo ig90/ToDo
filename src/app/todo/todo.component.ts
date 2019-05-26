bb71b36cbda2377e575709c55ad92d7d0bb4fdb1
Igor <igor.filipiuk@gmail.com>
2019-05-26 02:26:32 +0200

    added crossOut and getDate methods

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,20 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo.component.sass'],
 
 })
+
 export class TodoComponent {
 
   items: any[] = [
-    'task 1',
-    'task 2',
-    'task 3',
-    'task 4',
-    'task 5',
-    'task 6'
+    {title: 'task1', isChecked: false, date: this.getDate()},
+    {title: 'task2', isChecked: false, date: this.getDate()},
+    {title: 'task3', isChecked: false, date: this.getDate()},
+    {title: 'task4', isChecked: false, date: this.getDate()},
+    {title: 'task5', isChecked: false, date: this.getDate()},
+    {title: 'task6', isChecked: false, date: this.getDate()},
+
   ];
 
   addItem(input: HTMLInputElement) {
-    this.items.splice(0, 0, input.value);
+    const item = {
+      title: input.value,
+      isChecked: false,
+      date: this.getDate()
+    };
+    this.items.splice(0, 0, item);
     input.value = '';
+    console.log(item);
   }
 
   removeItem(item) {
@@ -27,4 +35,30 @@ export class TodoComponent {
     this.items.splice(index, 1);
   }
 
+  crossOut(item) {
+
+    if (item.isChecked === true) {
+      this.removeItem(item);
+      this.items.splice(0, 0, item);
+    } else {
+      this.items.splice(this.items.length, 1, item);
+      this.removeItem(item);
+    }
+    item.isChecked = !item.isChecked;
+  }
+
+  getDate() {
+
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getUTCDate();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+     return ''+ day+'/'+(month+1)+'/'+year+' '+hour+':'+minutes+':'+seconds;
+   }
+
 }
+
